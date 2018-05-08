6a52986df050e74852211b26cc320a6826176135
Carmelo <haotian.xue@msparis.com>
2018-05-08 18:56:34 +0800

    httpservice comments

--- util/httpService.js
+++ util/httpService.js
@@ -1,3 +1,6 @@
+/** http Service 封装后的使用方法与axios一致。
+** 后续的timeout,公共的headers,将通过config/config.js文件引入 
+*/
 import {baseUrl} from '/config/config.js';
 
 const parseUrl = (baseUrl,queryStringObject)=>{
@@ -17,17 +20,69 @@ const parseUrl = (baseUrl,queryStringObject)=>{
 }
 
 export const get = (url,config)=>{
-    let requstConfig = {
-        method:'get',
-        url:parseUrl(baseUrl,config.params),
-        headrs:config.headrs || {}
-    };
+    return new Promise((resolve,reject)=>{
+        let requstConfig = {
+                method:'get',
+                url:parseUrl(baseUrl,config.params),
+                headrs:config.headrs || {},
+                success:(...arg)=>{resolve(...arg)},
+                fail:(...arg)=>{reject(...arg)}
+            };
+        my.httpRequest(requstConfig);
+    })
 }
 
-const post = (url,data,config)=>{}
+export const post = (url,data,config)=>{
+    return new Promise((resolve,reject)=>{
+        let requstConfig = {
+                method:'post',
+                url:parseUrl(baseUrl,config.params),
+                headrs:config.headrs || {},
+                data,
+                success:(...arg)=>{resolve(...arg)},
+                fail:(...arg)=>{reject(...arg)}
+            };
+        my.httpRequest(requstConfig);
+    })
+}
 
-const put = (url,data,config)=>{}
+export const put = (url,data,config)=>{
+    return new Promise((resolve,reject)=>{
+        let requstConfig = {
+                method:'put',
+                url:parseUrl(baseUrl,config.params),
+                headrs:config.headrs || {},
+                data,
+                success:(...arg)=>{resolve(...arg)},
+                fail:(...arg)=>{reject(...arg)}
+            };
+        my.httpRequest(requstConfig);
+    })
+}
 
-const del = (url,params)=>{}
+export const del = (url,params)=>{
+    return new Promise((resolve,reject)=>{
+        let requstConfig = {
+                method:'delete',
+                url:parseUrl(baseUrl,config.params),
+                headrs:config.headrs || {},
+                data,
+                success:(...arg)=>{resolve(...arg)},
+                fail:(...arg)=>{reject(...arg)}
+            };
+        my.httpRequest(requstConfig);
+    })
+}
 
-const request = (config)=>{}
+export const request = (config)=>{
+    return new Promise((resolve,reject)=>{
+        if(!config.success){
+            config.success = (...arg)=>{resolve(arg)};
+        }
+         if(!config.fail){
+            config.success = (...arg)=>{reject(arg)};
+        }
+        
+        my.httpRequest(config);
+    })
+}
