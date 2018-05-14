7f548352a637b53e0cf18a390a884f8041439dba
liangsheng <906443185@qq.com>
2018-05-14 16:18:21 +0800

    物流部分

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -2,11 +2,12 @@ require('./config$');
 
 function success() {
 require('../..//app');
+require('../..//page/order/order');
+require('../..//page/logistics/logistics');
 require('../..//page/orderDetail/orderDetail');
 require('../..//page/rent/rent');
 require('../..//page/cart/cart');
 require('../..//page/user/user');
-require('../..//page/order/order');
 require('../..//page/bill/bill');
 require('../..//page/helpCenter/helpCenter');
 require('../..//page/buyCard/buyCard');
