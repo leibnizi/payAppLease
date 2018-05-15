24803babfc82d73d74beb10864ba8c9b1561647d
Carmelo <haotian.xue@msparis.com>
2018-05-15 17:33:19 +0800

    remove console

--- templates/couponList/couponList.js
+++ templates/couponList/couponList.js
@@ -6,14 +6,12 @@ export default {
     },
     op:{
         onClose:(page)=>{
-            console.log(page,"ahah")
             page.setData({
                 hidden:!page.data.hidden
             })
         },
         onReceive:(e,page)=>{
             const newIndex = page.data.currentIndex === e.currentTarget.dataset.index?null:e.currentTarget.dataset.index
-
             page.setData({
                 currentSelect:e.currentTarget.dataset.id,
                 currentIndex:newIndex
