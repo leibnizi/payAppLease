7996bafe8dde25422ddd0b70230ab5c21e9c0dc7
Carmelo <haotian.xue@msparis.com>
2018-05-15 11:58:52 +0800

    order

--- page/orderSuccess/orderSuccess.js
+++ page/orderSuccess/orderSuccess.js
@@ -5,7 +5,9 @@ Page({
         orderDetail: "查看订单",
         home:"返回首页",
         successImage: `${imagePath}/orderResult/success@2x.png`,
-        buttomImage: `${imagePath}/orderResult/buttom@2x.png`
+        buttomImage: `${imagePath}/orderResult/buttom@2x.png`,
+        noPayImage:`${imagePath}/orderResult/zhima@2x.png`,
+        noPay:true
     },
     onLoad() {
     },
